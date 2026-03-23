d513ac44c108001e6b888dd380cad4a28c9c02e8
Maymanaf <med.aymen.naf@gmail.com>
2026-03-23 16:57:49 +0100

    fixup! front: handle failed requests in e2e tests

--- front/tests/02-operational-studies/nge/002-nge-osrd.spec.ts
+++ front/tests/02-operational-studies/nge/002-nge-osrd.spec.ts
@@ -25,8 +25,6 @@ const frTranslations = {
 };
 
 test.describe('@op @nge @round-trips', () => {
-  test.use({ ignorePageErrors: true });
-
   let project: Project;
   let study: Study;
   let scenarioItems: Scenario;
