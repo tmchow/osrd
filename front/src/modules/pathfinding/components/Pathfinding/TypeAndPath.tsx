403799b61007c467b121f387781d540e7675e0e9
Theo Macron <theo.macron0315@gmail.com>
2026-03-27 16:15:15 +0100

    front: clear dropdown when the rocket is clicked
    Signed-off-by: Theo Macron <theo.macron0315@gmail.com>

--- front/src/modules/pathfinding/components/Pathfinding/TypeAndPath.tsx
+++ front/src/modules/pathfinding/components/Pathfinding/TypeAndPath.tsx
@@ -184,6 +184,8 @@ const TypeAndPath = ({
       setDisplayTypeAndPath?.(false);
       launchPathfinding(pathSteps);
       setInputText('');
+      setSearchResults([]);
+      setSearch('');
     }
   };
 
